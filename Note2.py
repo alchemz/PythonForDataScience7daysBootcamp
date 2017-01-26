50ededceb699e86c2dc46ea8dd91b534ec7eaa6c
xianling zhang <alchemxz@gmail.com>
2017-01-25 22:39:40 -0500

    Create Note2.py

---
+++ Note2.py
@@ -0,0 +1,13 @@
+seq=[1,2,3,4,5]
+for item in seq:
+  print (item);
+  print ("Hello")
+
+i=1 
+while i<5:
+  print('i is :{}',format(i)) #i is always smaller than 5
+  i=i+1 #otherwise inifite loop
+  
+for x in seq:
+  print(x)
+  
